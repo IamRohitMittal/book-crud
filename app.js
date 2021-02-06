9e342980fc5c001795728c00babf21caf210e6ec
Rohit <rohit@MTXs-MacBook-Pro.local>
2021-02-06 17:32:48 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const db=require('./models/db');
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser= require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.post('/book',(req,res)=>{
     const {name,releaseDate,authorName}=req.body;
-    db.book.insert({name:name,releaseDate:releaseDate,authorName:authorName});
+    db.book.create({
+		name : name,
+		releaseDate : releaseDate,
+		authorName : authorName
+	}, function(err, objBook) {
+		if (err)
+			res.send(err);
+ 
+		// get and return all the employees after newly created employe record
+		db.book.find(function(err, books) {
+			if (err)
+				res.send(err)
+			res.json(books);
+		});
+	});
 
 })
 
 app.put('/book/:uuid',(req,res)=>{
-    let book=db.book.find(b=>b.uuid==req.params.uuid)
+    let book=db.book.findByIdAndUpdate(b=>b.uuid==req.params.uuid)
 })
 
 app.delete('/book/:uuid',(req,res)=>{
@@ -20,11 +34,22 @@ app.delete('/book/:uuid',(req,res)=>{
 })
 
 app.get('/book',(req,res)=>{
-    db.book.
+    db.book.find(function(err, books) {
+		// if there is an error retrieving, send the error otherwise send data
+		if (err)
+			res.send(err)
+		res.json(books); // return all employees in JSON format
+	});
 })
 
 app.get('/book/:uuid',(req,res)=>{
-
+    let uuid = req.params.uuid;
+	db.books.findById(id, function(err, book) {
+		if (err)
+			res.send(err)
+ 
+		res.json(book);
+	});
 })
 // Create a listener for the port
 app.listen(5050, () => {
